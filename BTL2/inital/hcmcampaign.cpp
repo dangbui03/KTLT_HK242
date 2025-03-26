1716f64b79d9ab6d73bfebd04dcf304b6d2ce91e
DarkChan <buihohaidang@gmail.com>
2025-03-26 11:47:28 +0700

    Refactor Infantry and Vehicle classes for improved attack score calculations and add Army management functionality

--- BTL2/inital/hcmcampaign.cpp
+++ BTL2/inital/hcmcampaign.cpp
@@ -5,10 +5,12 @@
 ////////////////////////////////////////////////////////////////////////
 
 // Kiểm tra có phải số chính phương không
-bool isPerfectSquare(int n) {
-    if (n < 0) return false; // Số âm không thể là số chính phương
+bool isPerfectSquare(int n)
+{
+    if (n < 0)
+        return false;                      // Số âm không thể là số chính phương
     int sqrtN = static_cast<int>(sqrt(n)); // Lấy căn bậc hai của n
-    return (sqrtN * sqrtN == n); // Kiểm tra nếu căn bậc hai đã làm tròn lại bình phương ra số ban đầu
+    return (sqrtN * sqrtN == n);           // Kiểm tra nếu căn bậc hai đã làm tròn lại bình phương ra số ban đầu
 }
 
 #pragma region Position
@@ -67,20 +69,6 @@ string Unit::str() const
 };
 #pragma endregion Unit
 
-// class Tank : public Unit {
-// public:
-//     Tank(int quantity, int weight, Position pos) : Unit(quantity, weight, pos) {}
-//     int getAttackScore() override { return quantity * weight * 5; }
-//     string str() const override { return "Tank:" + pos.str() + ":" + to_string(quantity); }
-// };
-
-// class Soldier : public Unit {
-// public:
-//     Soldier(int quantity, int weight, Position pos) : Unit(quantity, weight, pos) {}
-//     int getAttackScore() override { return quantity * weight * 2; }
-//     string str() const override { return "Soldier:" + pos.str() + ":" + to_string(quantity); }
-// }
-
 class Vehicle : public Unit
 {
 private:
@@ -120,6 +108,10 @@ public:
     {
         int typeValue = vehicleType;
         int score = (typeValue * 304 + quantity * weight) / 30;
+        if (score > 1000)
+            score = 1000;
+        else if (score < 0)
+            score = 0;
         return score; // Round to threshold if exceeds
     }
 
@@ -131,78 +123,135 @@ public:
 
 class Infantry : public Unit
 {
-    private:
-        InfantryType infantryType;
+private:
+    InfantryType infantryType;
 
-        // tìm tổng các chữ số của một số
-        int sumOfDigits(int num) {
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
-            }
-            return sum;
+    // tìm tổng các chữ số của một số
+    int sumOfDigits(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    // Tìm số cá nhân
+    int getPersonalScore(int score)
+    {
+        int personalScore = sumOfDigits(score) + sumOfDigits(1975);
+        while (personalScore > 9)
+        {
+            personalScore = sumOfDigits(personalScore);
+        }
+        return personalScore;
+    }
+
+    string infantryTypeToString(InfantryType type) const
+    {
+        switch (type)
+        {
+        case SNIPER:
+            return "SNIPER";
+        case ANTIAIRCRAFTSQUAD:
+            return "ANTIAIRCRAFTSQUAD";
+        case MORTARSQUAD:
+            return "MORTARSQUAD";
+        case ENGINEER:
+            return "ENGINEER";
+        case SPECIALFORCES:
+            return "SPECIALFORCES";
+        case REGULARINFANTRY:
+            return "REGULARINFANTRY";
+        default:
+            return "Unknown InfantryType";
+        }
+    }
+
+public:
+    Infantry(int quantity, int weight, const Position pos, InfantryType infantryType) : Unit(quantity, weight, pos), infantryType(infantryType) {}
+
+    ~Infantry() {}
+
+    int getAttackScore() override
+    {
+        int typeValue = infantryType;
+        int score = typeValue * 56 + quantity * weight;
+
+        if ((infantryType == SPECIALFORCES) && isPerfectSquare(weight))
+        {
+            score += 75;
         }
 
-        // Tìm số cá nhân
-        int getPersonalScore(int score) {
-            int personalScore = sumOfDigits(score) + sumOfDigits(1975);
-            while (personalScore > 9) {
-                personalScore = sumOfDigits(personalScore);
-            }
-            return personalScore;
+        int personalScore = getPersonalScore(score);
+        if (personalScore > 7)
+        {
+            quantity = (int)ceil(quantity * 1.2);
+            score = typeValue * 56 + quantity * weight;
+        }
+        else if (personalScore < 4)
+        {
+            quantity = (int)ceil(quantity * 0.9);
         }
 
-        string infantryTypeToString(InfantryType type) const {
-            switch (type) {
-                case SNIPER:
-                    return "SNIPER";
-                case ANTIAIRCRAFTSQUAD:
-                    return "ANTIAIRCRAFTSQUAD";
-                case MORTARSQUAD:
-                    return "MORTARSQUAD";
-                case ENGINEER:
-                    return "ENGINEER";
-                case SPECIALFORCES:
-                    return "SPECIALFORCES";
-                case REGULARINFANTRY:
-                    return "REGULARINFANTRY";
-                default:
-                    return "Unknown InfantryType";
-            }
-        }
+        if (score > 500)
+            score = 500;
+        else if (score < 0)
+            score = 0;
+        return score;
+    }
 
-    public:
-        Infantry(int quantity , int weight , const Position pos , InfantryType infantryType) :
-            Unit(quantity , weight , pos) , infantryType(infantryType) {}
-
-        ~Infantry() {}
-
-        int getAttackScore() override {
-            int typeValue = infantryType;
-            int score = typeValue * 56 + quantity * weight;
-
-            if ((infantryType == SPECIALFORCES) && isPerfectSquare(weight)) {
-                score += 75;
-            }
-
-            int personalScore = getPersonalScore(score);
-            if (personalScore > 7) {
-                quantity = (int)ceil(quantity * 1.2);
-                score = typeValue * 56 + quantity * weight;
-            } else if (personalScore < 4) {
-                quantity = (int)ceil(quantity * 0.9);
-            }
-            return score;
-        }
-
-        string str() const override {
-            return "Infantry[" + infantryTypeToString(infantryType) + '=' + to_string(infantryType) + "," + to_string(quantity) + "," + to_string(weight) + "," + pos.str() + "]";
-        }
+    string str() const override
+    {
+        return "Infantry[" + infantryTypeToString(infantryType) + '=' + to_string(infantryType) + "," + to_string(quantity) + "," + to_string(weight) + "," + pos.str() + "]";
+    }
 };
 
-class Army {
-    
+Army::Army(Unit **unitArray, int size, string name, BattleField *battleField) : LF(0), EXP(0), name(name), unitList(nullptr), battleField(battleField)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Vehicle *vehicle = dynamic_cast<Vehicle *>(unitArray[i]);
+        if (vehicle != nullptr)
+        {
+            LF += vehicle->getAttackScore();
+        }
+        else
+        {
+            Infantry *infantry = dynamic_cast<Infantry *>(unitArray[i]);
+            if (infantry != nullptr)
+            {
+                EXP += infantry->getAttackScore();
+            }
+        }
+    }
+}
+
+Army::~Army()
+{
+    delete unitList;
+    delete battleField;
+}
+
+class LiberationArmy : public Army
+{
+public:
+    LiberationArmy(const Unit **unitArray, int size, string name, BattleField *battleField) : Army(const_cast<Unit **>(unitArray), size, name, battleField) {}
+    void fight(Army *enemy, bool defense = false) override
+    {
+        if (defense)
+        {
+            this->LF = LF * 1.3;
+            this->EXP = EXP * 1.3;
+        }
+        else
+        {
+            this->LF = LF * 1.5;
+            this->EXP = EXP * 1.5;
+        }
+    }
 };
 
 ////////////////////////////////////////////////
